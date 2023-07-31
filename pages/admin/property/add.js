9209283d5738ee8cbc418da59714e98fc1483261
Abhinandan Jhamb <77092975+iam5abhi@users.noreply.github.com>
2023-07-31 19:02:36 +0530

    Add files via upload
    add property

--- pages/admin/property/add.js
+++ pages/admin/property/add.js
@@ -6,16 +6,16 @@ import PrivateRoute from "../../../PrivateRoute/PrivateRoute";
 const AddContact = () => {
   const router = useRouter();
   const [formData, setFormData] = useState({
-    ProjectName:'',
-    Sector:'',
+    ProjectName:'', //done
+    Sector:'', //done
     ProjectPhotos:'',
-    ProjectBrochure:'',
-    PriceStartsfrom:'',
-    PricePerSQFT:'',
+    ProjectBrochure:'', 
+    PriceStartsfrom:'', //done
+    PricePerSQFT:'', //done
     AboutProject:'',
-    Nooffloors:'',
-    BasicAmenities:'',
-    AvailableFrom:'',
+    Nooffloors:'', //done
+    BasicAmenities:'', //done
+    AvailableFrom:'', //done
   });
 
   const onChangeHandler = (event) => {
@@ -41,7 +41,7 @@ const AddContact = () => {
       <div className="max-w-screen mx-auto">
         <div className="container mx-auto py-10">
           <div className="p-4">
-            <div className="w-full mx-auto md:w-[35%]">
+            <div className="w-full mx-auto md:w-[50%]">
               <form
                 onSubmit={handleSubmit}
                 className="bg-white shadow-xl py-14 rounded-lg p-5"
@@ -78,15 +78,68 @@ const AddContact = () => {
                         <label htmlfor="specialization" className="block text-sm font-medium text-gray-900 ">No</label>
                     </div>
                   </div>
-                  <div className="mb-4 flex gap-5">
+                  <label htmlfor="specialization" className="block text-sm font-medium text-gray-900 ">Basic Amenities</label>
+                  <div className="mb-4 grid grid-cols-3">
                     <div className="flex gap-1">
-                        <input type="radio" id="name" name='requirement' value="buy" onChange={onChangeHandler} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 " required />
-                        <label htmlfor="specialization" className="block text-sm font-medium text-gray-900 ">Sell</label>
+                        <input type="checkbox" id="name" name='requirement' value="buy" onChange={onChangeHandler} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 " required />
+                        <label htmlfor="specialization" className="block text-sm font-medium text-gray-900 ">Swimming Pool</label>
                     </div>
                     <div className="flex gap-1">
-                        <input type="radio" id="name" name='requirement' value="lease" onChange={onChangeHandler} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 " required />
-                        <label htmlfor="specialization" className="block text-sm font-medium text-gray-900 ">Lease</label>
+                        <input type="checkbox" id="name" name='requirement' value="RainWaterHarvesting" onChange={onChangeHandler} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 " required />
+                        <label htmlfor="specialization" className="block text-sm font-medium text-gray-900 ">Rain Water Harvesting</label>
                     </div>
+                    <div className="flex gap-1">
+                        <input type="checkbox" id="name" name='requirement' value="Security" onChange={onChangeHandler} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 " required />
+                        <label htmlfor="specialization" className="block text-sm font-medium text-gray-900 ">24x7 Security</label>
+                    </div>
+                    <div className="flex gap-1">
+                        <input type="checkbox" id="name" name='requirement' value="ClubHouse" onChange={onChangeHandler} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 " required />
+                        <label htmlfor="specialization" className="block text-sm font-medium text-gray-900 ">Club House</label>
+                    </div>
+                    <div className="flex gap-1">
+                        <input type="checkbox" id="name" name='requirement' value="WaterSupply" onChange={onChangeHandler} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 " required />
+                        <label htmlfor="specialization" className="block text-sm font-medium text-gray-900 ">24/7 Water Supply</label>
+                    </div>
+                    <div className="flex gap-1">
+                        <input type="checkbox" id="name" name='requirement' value="Lifts" onChange={onChangeHandler} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 " required />
+                        <label htmlfor="specialization" className="block text-sm font-medium text-gray-900 ">Lift(s)</label>
+                    </div> 
+                    <div className="flex gap-1">
+                        <input type="checkbox" id="name" name='requirement' value="CarParking" onChange={onChangeHandler} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 " required />
+                        <label htmlfor="specialization" className="block text-sm font-medium text-gray-900 ">Car Parking</label>
+                    </div> 
+                    <div className="flex gap-1">
+                        <input type="checkbox" id="name" name='requirement' value="Internal Street Lights" onChange={onChangeHandler} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 " required />
+                        <label htmlfor="specialization" className="block text-sm font-medium text-gray-900 ">Internal Street Lights</label>
+                    </div> 
+                    <div className="flex gap-1">
+                        <input type="checkbox" id="name" name='requirement' value="Fire Fighting Systems" onChange={onChangeHandler} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 " required />
+                        <label htmlfor="specialization" className="block text-sm font-medium text-gray-900 ">Fire Fighting Systems</label>
+                    </div> 
+                  </div> 
+                  <div className="mb-4">
+                    <label
+                      htmlFor="category"
+                      className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                      Select an option
+                    </label>
+                    <select
+                      id="AvailableFrom"
+                      name="AvailableFrom"
+                      onChange={onChangeHandler}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                      required
+                    >
+                      <option value="">Choose</option>
+                      <option value="">2024</option>
+                      <option value="">2025</option>
+                      <option value="">2026</option>
+                      <option value="">2027</option>
+                      <option value="">2028</option>
+                      <option value="">2029</option>
+                      <option value="">2030</option>
+                    </select>
                   </div>
                   <div className="text-center mb-6">
                     <button
