a16a98f2288849d92a48dd67dad98b5d6f4fbd93
iam5abhi <support@codesoftic.com>
2023-08-02 18:47:52 +0530

    main

--- components/QueryForm/QueryFrom.js
+++ components/QueryForm/QueryFrom.js
@@ -75,7 +75,7 @@ export default function QueryFrom({setOpen,open,id}) {
                               </div>
                               <div className='mt-1'>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email <span class="text-red-600">*</span></label>
-                                <input type="text" name='email' onChange={handleChange} id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder='Enter Email' required/>
+                                <input type="text" name='email' onChange={handleChange} id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder='Enter Email' />
                               </div>
                               <div className='mt-1'>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number <span class="text-red-600">*</span></label>
