bfb178255609819850cdbf4b7eb1c6277b5f2606
Abhinandan Jhamb <77092975+iam5abhi@users.noreply.github.com>
2023-08-02 10:58:49 +0530

    Add files via upload
    done all

--- components/QueryForm/QueryFrom.js
+++ components/QueryForm/QueryFrom.js
@@ -44,7 +44,7 @@ export default function QueryFrom({setOpen,open,id}) {
         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
       </Transition.Child>
       <div className="fixed inset-0 z-10 overflow-y-auto">
-        <div className="flex md:min-h-full mt-36 items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <div className="flex md:min-h-full mt-36 md:mt-0 items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -54,7 +54,7 @@ export default function QueryFrom({setOpen,open,id}) {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <Dialog.Panel className="relative transform overflow-hidden rounded-xl bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full md:max-w-xl">
+            <Dialog.Panel className="relative transform overflow-hidden rounded-xl bg-white text-left shadow-xl transition-all sm:my-8 w-full md:max-w-xl">
             <div className="max-w-screen mx-auto">
               <div className="container mx-auto">
                 <div className="col-span-2">
