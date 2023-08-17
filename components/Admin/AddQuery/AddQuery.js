0c11d975fc85764484d7243f4a81284d1a778346
iam5abhi <support@codesoftic.com>
2023-08-17 17:40:08 +0530

    main

--- components/Admin/AddQuery/AddQuery.js
+++ components/Admin/AddQuery/AddQuery.js
@@ -90,7 +90,7 @@ export default function AddQuery({setOpen,open,getQueriesData}) {
                                 <select id="AvailableFrom" name="ProjectID" onChange={handleChange}
                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required >
                                   <option value="">Choose Project</option>
-                                  {projects.map((data)=><option value={data.id}>{data.ProjectName}({data.requirement})({data.PriceStartsfrom})</option>)}
+                                  {projects.map((data)=><option key={data.id} value={data.id}>{data.ProjectName}({data.requirement})({data.PriceStartsfrom})</option>)}
                                   
                                 </select>
                               </div>
