3d2547cc5e1d672d1df865dcae314d77d6445961
iam5abhi <support@codesoftic.com>
2023-08-21 18:41:43 +0530

    main

--- pages/admin/queries/[id].js
+++ pages/admin/queries/[id].js
@@ -152,7 +152,7 @@ const UpdateContact = () => {
                 <div className="mb-4">
                     <label htmlFor="category" className="block mb-2 md:text-sm text-xs font-medium text-gray-900"> Select an Staus </label>
                     <select id="AvailableFrom" value={formData.status} name="status" onChange={onChangeHandler}
-                      className="bg-gray-50 border border-gray-300 text-gray-900 md:text-sm text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" >
+                      className="appearance-none bg-gray-50 border border-gray-300 text-gray-900 md:text-sm text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" >
                       <option value="">Choose Status</option>
                       <option className="bg-green-300" value="newlead">New Lead</option>
                       <option className="bg-yellow-300" value="inprogress">InProgress</option>
